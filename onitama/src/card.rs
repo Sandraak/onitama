03c03b6a68a75744479eda1faca3ad79ca6f407f
Sandra ter Maat <HS.terMaat@student.han.nl>
2022-06-15 16:37:14 +0200

    implemented game

--- onitama/src/card.rs
+++ onitama/src/card.rs
@@ -1,55 +1,89 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, Deserialize)]
 pub struct Card {
     animal: &'static str,
-    moves: Vec<Move>,
+    pub moves: Vec<Move>,
 }
 
 impl Card {
     pub fn deck() -> Vec<Card> {
-        let tiger = Card { animal: "Tiger", moves: vec![UPUP, DOWN] };
-        let dragon = Card { animal: "Dragon", moves: vec![UPLEFTLEFT, UPRIGHTRIGHT, DOWNLEFT, DOWNRIGHT] };
-        let frog = Card { animal: "Frog", moves: vec![UPLEFT, LEFTLEFT, DOWNRIGHT] };
-        let rabbit = Card { animal: "Rabbit", moves: vec![UPRIGHT, RIGHTRIGHT, DOWNLEFT] };
-        let crab = Card { animal: "Crab", moves: vec![UP, LEFTLEFT, RIGHTRIGHT] };
-        let elephant = Card { animal: "Elephant", moves: vec![UPLEFT, UPRIGHT, LEFT, RIGHT] };
-        let goose = Card { animal: "Goose", moves: vec![UPLEFT, LEFT, RIGHT, DOWNRIGHT] };
-        let rooster = Card { animal: "Rooster", moves: vec![UPRIGHT, LEFT, RIGHT, DOWNLEFT] };
-        let monkey = Card { animal: "Monkey", moves: vec![UPLEFT, UPRIGHT, DOWNLEFT, DOWNRIGHT] };
-        let mantis = Card { animal: "Mantis", moves: vec![UPLEFT, UPRIGHT, DOWN] };
-        let horse = Card { animal: "Horse", moves: vec![UP, LEFT, DOWN] };
-        let ox = Card { animal: "Ox", moves: vec![UP, RIGHT, DOWN] };
-        let crane = Card { animal: "Crane", moves: vec![UP, DOWNLEFT, DOWNRIGHT] };
-        let boar = Card { animal: "Boar", moves: vec![UP, LEFT, RIGHT] };
-        let eel = Card { animal: "Eel", moves: vec![UPLEFT, RIGHT, DOWNLEFT] };
-        let cobra = Card { animal: "Cobra", moves: vec![UPRIGHT, LEFT, DOWNRIGHT] };
+        let tiger = Card {
+            animal: "Tiger",
+            moves: vec![UPUP, DOWN],
+        };
+        let dragon = Card {
+            animal: "Dragon",
+            moves: vec![UPLEFTLEFT, UPRIGHTRIGHT, DOWNLEFT, DOWNRIGHT],
+        };
+        let frog = Card {
+            animal: "Frog",
+            moves: vec![UPLEFT, LEFTLEFT, DOWNRIGHT],
+        };
+        let rabbit = Card {
+            animal: "Rabbit",
+            moves: vec![UPRIGHT, RIGHTRIGHT, DOWNLEFT],
+        };
+        let crab = Card {
+            animal: "Crab",
+            moves: vec![UP, LEFTLEFT, RIGHTRIGHT],
+        };
+        let elephant = Card {
+            animal: "Elephant",
+            moves: vec![UPLEFT, UPRIGHT, LEFT, RIGHT],
+        };
+        let goose = Card {
+            animal: "Goose",
+            moves: vec![UPLEFT, LEFT, RIGHT, DOWNRIGHT],
+        };
+        let rooster = Card {
+            animal: "Rooster",
+            moves: vec![UPRIGHT, LEFT, RIGHT, DOWNLEFT],
+        };
+        let monkey = Card {
+            animal: "Monkey",
+            moves: vec![UPLEFT, UPRIGHT, DOWNLEFT, DOWNRIGHT],
+        };
+        let mantis = Card {
+            animal: "Mantis",
+            moves: vec![UPLEFT, UPRIGHT, DOWN],
+        };
+        let horse = Card {
+            animal: "Horse",
+            moves: vec![UP, LEFT, DOWN],
+        };
+        let ox = Card {
+            animal: "Ox",
+            moves: vec![UP, RIGHT, DOWN],
+        };
+        let crane = Card {
+            animal: "Crane",
+            moves: vec![UP, DOWNLEFT, DOWNRIGHT],
+        };
+        let boar = Card {
+            animal: "Boar",
+            moves: vec![UP, LEFT, RIGHT],
+        };
+        let eel = Card {
+            animal: "Eel",
+            moves: vec![UPLEFT, RIGHT, DOWNLEFT],
+        };
+        let cobra = Card {
+            animal: "Cobra",
+            moves: vec![UPRIGHT, LEFT, DOWNRIGHT],
+        };
 
         vec![
-            tiger,
-            dragon,
-            frog,
-            rabbit,
-            crab,
-            elephant,
-            goose,
-            rooster,
-            monkey,
-            mantis,
-            horse,
-            ox,
-            crane,
-            boar,
-            eel,
-            cobra,
+            tiger, dragon, frog, rabbit, crab, elephant, goose, rooster, monkey, mantis, horse, ox,
+            crane, boar, eel, cobra,
         ]
     }
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, Deserialize)]
 pub struct Move {
-    dx: i8,
-    dy: i8,
+    pub dx: i8,
+    pub dy: i8,
 }
 
 const UP: Move = Move { dx: 0, dy: -1 };
--- onitama/src/lib.rs
+++ onitama/src/lib.rs
@@ -1,1 +1,139 @@
+use std::{collections::BTreeMap, ops::Add};
+
+use card::{Card, Move};
+use rand::prelude::SliceRandom;
+
 pub mod card;
+pub struct State {
+    board: Board,
+    current_player: Colour,
+    spare_card: Card,
+    cards: BTreeMap<Colour, [Card; 2]>,
+}
+
+impl State {
+    fn new() -> Self {
+        let mut deck = Card::deck();
+        let mut cards = BTreeMap::new();
+        cards.insert(Colour::Blue, [deck.pop().unwrap(), deck.pop().unwrap()]);
+        cards.insert(Colour::Red, [deck.pop().unwrap(), deck.pop().unwrap()]);
+        deck.shuffle(&mut rand::thread_rng());
+        State {
+            board: Board::default(),
+            current_player: Colour::Red,
+            spare_card: deck.pop().unwrap(),
+            cards,
+        }
+    }
+
+    fn perform_mov(&mut self, mov: MovePiece) -> Result<(), MoveError> {
+        if !self
+            .cards
+            .get(&self.current_player)
+            .unwrap()
+            .iter()
+            .any(|card| card.moves.contains(&mov.mov))
+        {
+            return Err(MoveError::InvalidMove);
+        }
+        let piece = self.board.board[mov.from.x][mov.from.y]
+            .take()
+            .ok_or(MoveError::NoPiece)?;
+        let to: Position = Position {
+            x: (mov.from.x as i8 + mov.mov.dx) as usize,
+            y: (mov.from.y as i8 + mov.mov.dy) as usize,
+        };
+        self.board.board[to.x][to.y] = Some(piece);
+        Ok(())
+    }
+
+    fn winner(&self) -> Option<Colour> {
+        if !self
+            .board
+            .board
+            .iter()
+            .flatten()
+            .flatten()
+            .any(|piece| *piece == Piece::REDMASTER)
+        {
+            return Some(Colour::Blue);
+        } else if !self
+            .board
+            .board
+            .iter()
+            .flatten()
+            .flatten()
+            .any(|piece| *piece == Piece::BLUEMASTER)
+        {
+            return Some(Colour::Red);
+        }
+
+        if self.board.board[0][2] == Some(Piece::REDMASTER) {
+            return Some(Colour::Red);
+        } else if self.board.board[4][2] == Some(Piece::BLUEMASTER) {
+            return Some(Colour::Blue);
+        }
+        None
+    }
+}
+
+struct Board {
+    board: [[Option<Piece>; 5]; 5],
+}
+
+impl Board {}
+
+impl Default for Board {
+    fn default() -> Self {
+        let mut board: [[Option<Piece>; 5]; 5] = Default::default();
+        board[0][0] = Some(Piece::BLUEPAWN);
+        board[0][1] = Some(Piece::BLUEPAWN);
+        board[0][2] = Some(Piece::BLUEMASTER);
+        board[0][3] = Some(Piece::BLUEPAWN);
+        board[0][4] = Some(Piece::BLUEPAWN);
+
+        board[4][0] = Some(Piece::REDPAWN);
+        board[4][1] = Some(Piece::REDPAWN);
+        board[4][2] = Some(Piece::REDMASTER);
+        board[4][3] = Some(Piece::REDPAWN);
+        board[4][4] = Some(Piece::REDPAWN);
+        Board { board }
+    }
+}
+#[derive(PartialEq, Eq)]
+struct Piece(Rank, Colour);
+
+impl Piece {
+    const REDPAWN: Piece = Piece(Rank::Pawn, Colour::Red);
+    const REDMASTER: Piece = Piece(Rank::Master, Colour::Red);
+    const BLUEPAWN: Piece = Piece(Rank::Pawn, Colour::Blue);
+    const BLUEMASTER: Piece = Piece(Rank::Master, Colour::Blue);
+}
+
+struct MovePiece {
+    from: Position,
+    mov: Move,
+    card: usize, //index van de kaarten van de spelen
+}
+
+struct Position {
+    x: usize,
+    y: usize,
+}
+
+#[derive(Eq, PartialEq)]
+enum Rank {
+    Pawn,
+    Master,
+}
+
+#[derive(Ord, PartialOrd, PartialEq, Eq)]
+enum Colour {
+    Red,
+    Blue,
+}
+
+enum MoveError {
+    NoPiece,
+    InvalidMove,
+}
